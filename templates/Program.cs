9e33cf9ae6291f084201e5385ce10a0697bbccae
Steven Cantrell <Steven.Cantrell@gtri.gatech.edu>
2023-12-06 23:25:55 +0000

    template new proj

---
+++ templates/Program.cs
@@ -0,0 +1,15 @@
+static void PartOne(PartOneInput input)
+{
+    Console.WriteLine($"Part One Answer: ");
+}
+
+static void PartTwo(PartTwoInput input)
+{
+    Console.WriteLine($"Part Two Answer: ");
+}
+
+PartOneInput partOne = new(args[0]);
+PartOne(partOne);
+
+PartTwoInput partTwo = new(args[0]);
+PartTwo(partTwo);
