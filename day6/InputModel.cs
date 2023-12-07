b3cfcf538d579bd8413ec2ac3a1c1f39c388e916
Steven Cantrell <Steven.Cantrell@gtri.gatech.edu>
2023-12-07 01:48:56 +0000

    finished day6

--- day6/InputModel.cs
+++ day6/InputModel.cs
@@ -1,17 +1,61 @@
+class RaceStats(long time, long distance)
+{
+    public long Time { get; set; } = time;
+    public long Distance { get; set; } = distance;
+
+    public List<long> GetWinningDistances()
+    {
+        List<long> distances = [];
+        for (long holdTime = 0; holdTime < this.Time; holdTime++)
+        {
+            long speed = holdTime;
+            long remainingTime = this.Time - holdTime;
+            long distance = speed * remainingTime;
+            distances.Add(distance);
+        }
+        return distances.Where(t => t > this.Distance).ToList();
+    }
+}
+
 class PartOneInput
 {
+    public List<RaceStats> Stats { get; set ; } = [];
 
     public PartOneInput(string filepath)
     {
-
+        // Parse stats
+        string[] fileLines = File.ReadAllLines(filepath);
+        string[] timeSplit = fileLines[0].Split(' ', StringSplitOptions.TrimEntries)
+                                         .Skip(1)
+                                         .Where(s => !string.IsNullOrWhiteSpace(s))
+                                         .ToArray();
+        string[] distanceSplit = fileLines[1].Split(' ', StringSplitOptions.TrimEntries)
+                                             .Skip(1)
+                                             .Where(s => !string.IsNullOrWhiteSpace(s))
+                                             .ToArray();
+        for (int i = 0; i < timeSplit.Length; i++)
+        {
+            this.Stats.Add(new (long.Parse(timeSplit[i]), long.Parse(distanceSplit[i])));
+        }
     }
 }
 
 class PartTwoInput
 {
+    public RaceStats Stats { get; set ; }
 
     public PartTwoInput(string filepath)
     {
-
+        // Parse stats
+        string[] fileLines = File.ReadAllLines(filepath);
+        string timeStr = fileLines[0].Split(' ', StringSplitOptions.TrimEntries)
+                                     .Skip(1)
+                                     .Where(s => !string.IsNullOrWhiteSpace(s))
+                                     .Aggregate((s1, s2) => $"{s1}{s2}");
+        string distStr = fileLines[1].Split(' ', StringSplitOptions.TrimEntries)
+                                     .Skip(1)
+                                     .Where(s => !string.IsNullOrWhiteSpace(s))
+                                     .Aggregate((s1, s2) => $"{s1}{s2}");
+        this.Stats = new(long.Parse(timeStr), long.Parse(distStr));
     }
 }
--- day6/Program.cs
+++ day6/Program.cs
@@ -1,15 +1,29 @@
 static void PartOne(PartOneInput input)
 {
-    Console.WriteLine($"Part One Answer: ");
+    long marginOfError = 1;
+    foreach (var stat in input.Stats)
+    {
+        var winningDistances = stat.GetWinningDistances();
+        marginOfError *= winningDistances.Count;
+    }
+    Console.WriteLine($"Part One Answer: {marginOfError}");
 }
 
 static void PartTwo(PartTwoInput input)
 {
-    Console.WriteLine($"Part Two Answer: ");
+    var winningDistances = input.Stats.GetWinningDistances();
+    Console.WriteLine($"Part Two Answer: {winningDistances.Count}");
 }
 
+Console.WriteLine("Running part one...");
 PartOneInput partOne = new(args[0]);
+var watch = System.Diagnostics.Stopwatch.StartNew();
 PartOne(partOne);
+Console.WriteLine($"Part one finished: {watch.ElapsedMilliseconds}ms");
 
+Console.WriteLine("\nRunning part two...");
+watch = System.Diagnostics.Stopwatch.StartNew();
 PartTwoInput partTwo = new(args[0]);
 PartTwo(partTwo);
+watch.Stop();
+Console.WriteLine($"Part two finished: {watch.ElapsedMilliseconds}ms");
