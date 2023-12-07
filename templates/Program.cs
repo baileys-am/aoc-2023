af4fb22978da7ba1149ac1bd214b5ea5b7b77cb1
Steven Cantrell <Steven.Cantrell@gtri.gatech.edu>
2023-12-07 01:49:21 +0000

    add timing

--- templates/Program.cs
+++ templates/Program.cs
@@ -8,8 +8,15 @@ static void PartTwo(PartTwoInput input)
     Console.WriteLine($"Part Two Answer: ");
 }
 
+Console.WriteLine("Running part one...");
 PartOneInput partOne = new(args[0]);
+var watch = System.Diagnostics.Stopwatch.StartNew();
 PartOne(partOne);
+Console.WriteLine($"Part one finished: {watch.ElapsedMilliseconds}ms");
 
+Console.WriteLine("\nRunning part two...");
+watch = System.Diagnostics.Stopwatch.StartNew();
 PartTwoInput partTwo = new(args[0]);
 PartTwo(partTwo);
+watch.Stop();
+Console.WriteLine($"Part two finished: {watch.ElapsedMilliseconds}ms");
