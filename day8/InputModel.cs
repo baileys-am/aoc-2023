6c6b7c3d94a453a566f0689cc95c86d08a2d7bf5
Steven Cantrell <Steven.Cantrell@gtri.gatech.edu>
2023-12-08 15:51:17 +0000

    day8 part2 completed

--- day8/InputModel.cs
+++ day8/InputModel.cs
@@ -8,7 +8,6 @@ class PartOneInput
 {
     public string Instructions { get; set; }
     public Dictionary<string, Node> Nodes { get; set; } = [];
-    public string StartNodeId { get; set; } = "";
 
     public PartOneInput(string filepath)
     {
@@ -20,10 +19,6 @@ class PartOneInput
         {
             var lineSplit = fileLines[i].Split('=', StringSplitOptions.TrimEntries);
             var nodeId = lineSplit[0];
-            if (i == 2)
-            {
-                this.StartNodeId = nodeId;
-            }
             var lrSplit = lineSplit[1].Split([",", "(", ")"], StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
             this.Nodes[nodeId] = new(lrSplit[0], lrSplit[1]);
         }
@@ -32,10 +27,21 @@ class PartOneInput
 
 class PartTwoInput
 {
+    public string Instructions { get; set; }
+    public Dictionary<string, Node> Nodes { get; set; } = [];
 
     public PartTwoInput(string filepath)
     {
         // Parse
         string[] fileLines = File.ReadAllLines(filepath);
+        this.Instructions = fileLines[0];
+
+        for (int i = 2; i < fileLines.Length; i++)
+        {
+            var lineSplit = fileLines[i].Split('=', StringSplitOptions.TrimEntries);
+            var nodeId = lineSplit[0];
+            var lrSplit = lineSplit[1].Split([",", "(", ")"], StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            this.Nodes[nodeId] = new(lrSplit[0], lrSplit[1]);
+        }
     }
 }
--- day8/Program.cs
+++ day8/Program.cs
@@ -22,7 +22,45 @@ static void PartOne(PartOneInput input)
 
 static void PartTwo(PartTwoInput input)
 {
-    Console.WriteLine($"Part Two Answer: ");
+    // Thank you SO: https://stackoverflow.com/questions/147515/least-common-multiple-for-3-or-more-numbers/29717490#29717490
+    static long LCM(long[] numbers)
+    {
+        return numbers.Aggregate(lcm);
+    }
+    static long lcm(long a, long b)
+    {
+        return Math.Abs(a * b) / GCD(a, b);
+    }
+    static long GCD(long a, long b)
+    {
+        return b == 0 ? a : GCD(b, a % b);
+    }
+
+    char start = 'A';
+    char end = 'Z';
+    var startNodes = input.Nodes.Keys.Where(k => k[2] == start).ToList();
+    Dictionary<string, long> cyclicLUT = [];
+    foreach (var node in startNodes)
+    {
+        string currentPosition = node;
+        int currentInstruction = 0;
+        long stepCount = 0;
+        while (currentPosition[2] != end)
+        {
+            stepCount++;
+            currentPosition = input.Instructions[currentInstruction] switch
+            {
+                'L' => input.Nodes[currentPosition].LeftNode,
+                'R' => input.Nodes[currentPosition].RightNode,
+                _ => throw new Exception("oops!")
+            };
+            currentInstruction = currentInstruction + 1 >= input.Instructions.Length ? 0 : currentInstruction + 1;
+        }
+        cyclicLUT[node] = stepCount;
+    }
+
+    long overallStepCount = LCM([.. cyclicLUT.Values]);
+    Console.WriteLine($"Part Two Answer: {overallStepCount}");
 }
 
 Console.WriteLine("Running part one...");
